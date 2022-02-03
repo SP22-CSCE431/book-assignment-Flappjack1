f736994e221c8161a5ec86d51f078fd10b9c8fac
root <root@localhost>
2022-02-03 20:51:30 +0000

    Final Book Collection

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,9 +4,44 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Harry Potter'
+    fill_in 'Author', with: 'J.K Rowling'
+    fill_in 'Price', with: '15.00'
+    select '2001', from: 'book_published_date_1i'
+    select '01', from: 'book_published_date_2i'
+    select '01', from: 'book_published_date_3i'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry Potter')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'Dune'
+    fill_in 'Author', with: 'Frank Herbert'
+    fill_in 'Price', with: '25.00'
+    select '1965', from: 'book_published_date_1i'
+    select '05', from: 'book_published_date_2i'
+    select '20', from: 'book_published_date_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Dune')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'Percy Jackson'
+    fill_in 'Author', with: 'Rick Riordan'
+    fill_in 'Price', with: '20.00'
+    select '2005', from: 'book_published_date_1i'
+    select '08', from: 'book_published_date_2i'
+    select '15', from: 'book_published_date_3i'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Percy Jackson')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'Harry Potter', author: 'J.K Rowling', price: '15.00', published_date: '2001-01-01')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +12,43 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    subject.author = nil
+    subject.price = nil
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+end
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'Dune', author: 'Frank Herbert', price: '25.00', published_date: '1965-05-20')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    subject.author = nil
+    subject.price = nil
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+end
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'Percy Jackson', author: 'Rick Riordan', price: '20.00', published_date: '2005-08-15')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.title = nil
+    subject.author = nil
+    subject.price = nil
+    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
